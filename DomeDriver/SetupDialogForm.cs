2269f5c4ac28718b3c64fa3b926a894bfecf0ce0
LeoMannn <LeoMann@protonmail.com>
2024-01-30 19:56:10 +0100

    Add project files.

---
+++ DomeDriver/SetupDialogForm.cs
@@ -0,0 +1,156 @@
+using ASCOM.Utilities;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ASCOM.APS.Dome
+{
+    [ComVisible(false)] // Form not registered for COM!
+    public partial class SetupDialogForm : Form
+    {
+        const string NO_PORTS_MESSAGE = "Nessuna porta COM disponibile";
+        TraceLogger tl; // Holder for a reference to the driver's trace logger
+
+        bool closeForm = true;
+        public SetupDialogForm(TraceLogger tlDriver)
+        {
+            InitializeComponent();
+
+            // Save the provided trace logger for use within the setup dialogue
+            tl = tlDriver;
+
+            // Initialise current values of user settings from the ASCOM Profile
+            InitUI();
+        }
+
+        private void CmdOK_Click(object sender, EventArgs e) // OK button event handler
+        {
+            tl.Enabled = chkTrace.Checked;
+
+            DomeHardware.connectionMode = tabCtrl1.SelectedIndex;
+
+            if (DomeHardware.connectionMode == 0)
+            {
+                if (comboBoxComPort.Items.Count == 0)
+                {
+                    comboBoxComPort.SelectedItem = null;
+                    _ = MessageBox.Show("Nessuna porta disponibile", "Errore porta COM", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tl.LogMessage("Setup OK", $"New configuration values - NO COM ports detected on this machine");
+                    closeForm = false;
+                }
+                else if (string.IsNullOrEmpty((string)comboBoxComPort.SelectedItem))
+                {
+                    _ = MessageBox.Show("Nessuna porta selezionata", "Errore porta COM", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tl.LogMessage("Setup OK", $"New configuration values - COM Port: Not selected");
+                    closeForm = false;
+                }
+                else
+                {
+                    DomeHardware.comPort = (string)comboBoxComPort.SelectedItem;
+                    tl.LogMessage("Setup OK", $"New configuration values - COM Port: {DomeHardware.comPort}");
+                    closeForm = true;
+                }
+            }
+            else
+            {
+                string uri_s = txtIP.Text;
+
+                if (!uri_s.StartsWith("http://"))
+                {
+                    uri_s = "http://" + uri_s;
+                }
+
+                if (!Uri.TryCreate(uri_s, UriKind.Absolute, out _))
+                {
+                    _ = MessageBox.Show("Inserire un indirizzo IP valido", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tl.LogMessage("Setup OK", $"New configuration values - IP address: not valid");
+                    closeForm = false;
+                }
+                else
+                {
+                    DomeHardware.ipAddress = txtIP.Text;
+                    tl.LogMessage("Setup OK", $"New configuration values - IP address: {DomeHardware.ipAddress}");
+                    closeForm = true;
+                }
+            }
+        }
+
+        private void CmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
+        {
+            closeForm = true;
+            Close();
+        }
+
+        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://ascom-standards.org/");
+            }
+            catch (Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+
+        private void InitUI()
+        {
+
+            // Set the trace checkbox
+            chkTrace.Checked = tl.Enabled;
+
+            SetComPorts();
+
+            // select the current port if possible
+            if (comboBoxComPort.Items.Contains(DomeHardware.comPort))
+            {
+                comboBoxComPort.SelectedItem = DomeHardware.comPort;
+            }
+
+            txtIP.Text = DomeHardware.ipAddress;
+
+            tl.LogMessage("InitUI", $"Set UI controls to Trace: {chkTrace.Checked}, COM Port: {comboBoxComPort.SelectedItem}, IP Address: {txtIP.Text}");
+        }
+
+        private void SetupDialogForm_Load(object sender, EventArgs e)
+        {
+            // Bring the setup dialogue to the front of the screen
+            if (WindowState == FormWindowState.Minimized)
+                WindowState = FormWindowState.Normal;
+            else
+            {
+                TopMost = true;
+                Focus();
+                BringToFront();
+                TopMost = false;
+            }
+        }
+
+        private void SetupDialogForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = !closeForm;
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            SetComPorts();
+        }
+
+        private void SetComPorts()
+        {
+            // set the list of COM ports to those that are currently available
+            comboBoxComPort.Items.Clear(); // Clear any existing entries
+            using (Serial serial = new Serial()) // User the Se5rial component to get an extended list of COM ports
+            {
+                comboBoxComPort.Items.AddRange(serial.AvailableCOMPorts);
+            }
+            comboBoxComPort.SelectedIndex = 0;
+        }
+    }
+}
---
+++ DomeDriver/SetupDialogForm.designer.cs
@@ -0,0 +1,243 @@
+
+namespace ASCOM.APS.Dome
+{
+    partial class SetupDialogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SetupDialogForm));
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picASCOM = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkTrace = new System.Windows.Forms.CheckBox();
+            this.comboBoxComPort = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.tabCtrl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).BeginInit();
+            this.tabCtrl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.cmdOK.Location = new System.Drawing.Point(281, 112);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(59, 24);
+            this.cmdOK.TabIndex = 0;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.CmdOK_Click);
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdCancel.Location = new System.Drawing.Point(281, 142);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(59, 25);
+            this.cmdCancel.TabIndex = 1;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.CmdCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Scegliere la porta COM";
+            // 
+            // picASCOM
+            // 
+            this.picASCOM.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picASCOM.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picASCOM.Image = ((System.Drawing.Image)(resources.GetObject("picASCOM.Image")));
+            this.picASCOM.Location = new System.Drawing.Point(292, 9);
+            this.picASCOM.Name = "picASCOM";
+            this.picASCOM.Size = new System.Drawing.Size(48, 56);
+            this.picASCOM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picASCOM.TabIndex = 3;
+            this.picASCOM.TabStop = false;
+            this.picASCOM.Click += new System.EventHandler(this.BrowseToAscom);
+            this.picASCOM.DoubleClick += new System.EventHandler(this.BrowseToAscom);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Porta COM";
+            // 
+            // chkTrace
+            // 
+            this.chkTrace.AutoSize = true;
+            this.chkTrace.Location = new System.Drawing.Point(202, 150);
+            this.chkTrace.Name = "chkTrace";
+            this.chkTrace.Size = new System.Drawing.Size(69, 17);
+            this.chkTrace.TabIndex = 6;
+            this.chkTrace.Text = "Trace on";
+            this.chkTrace.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxComPort
+            // 
+            this.comboBoxComPort.FormattingEnabled = true;
+            this.comboBoxComPort.Location = new System.Drawing.Point(85, 40);
+            this.comboBoxComPort.Name = "comboBoxComPort";
+            this.comboBoxComPort.Size = new System.Drawing.Size(79, 21);
+            this.comboBoxComPort.TabIndex = 7;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(170, 40);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(59, 23);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Aggiorna";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // tabCtrl1
+            // 
+            this.tabCtrl1.Controls.Add(this.tabPage1);
+            this.tabCtrl1.Controls.Add(this.tabPage2);
+            this.tabCtrl1.Location = new System.Drawing.Point(12, 42);
+            this.tabCtrl1.Name = "tabCtrl1";
+            this.tabCtrl1.SelectedIndex = 0;
+            this.tabCtrl1.Size = new System.Drawing.Size(263, 102);
+            this.tabCtrl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnRefresh);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBoxComPort);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(255, 76);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Seriale";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.txtIP);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(255, 76);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Rete";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(13, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 30);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Scegliere la modalità di collegamento con APS Dome Control";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(21, 35);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(183, 20);
+            this.txtIP.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(7, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(245, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Inserire l\'indirizzo IP";
+            // 
+            // SetupDialogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 175);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tabCtrl1);
+            this.Controls.Add(this.chkTrace);
+            this.Controls.Add(this.picASCOM);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.cmdOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SetupDialogForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "APS Dome Control ASCOM Driver Setup";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SetupDialogForm_FormClosing);
+            this.Load += new System.EventHandler(this.SetupDialogForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).EndInit();
+            this.tabCtrl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picASCOM;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkTrace;
+        private System.Windows.Forms.ComboBox comboBoxComPort;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TabControl tabCtrl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+    }
+}
